00d4cc1b733538fdb91d89044271831fee045b0c
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2025-06-07 14:01:44 -0400

    Add icons

---
+++ src/components/widgets/icons/IconMake.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+  import { iconNameType } from '../../../helpers/type'
+
+  import DebugPath               from './paths/Debug.vue'
+  import KeyboardPath            from './paths/Keyboard.vue'
+  import RedoPath                from './paths/Redo.vue'
+  import SharePath               from './paths/Share.vue'
+  import TrashCanPath            from './paths/TrashCan.vue'
+  import UndoPath                from './paths/Undo.vue'
+
+  interface Props {
+    name: iconNameType
+    size?: number
+  }
+
+  const {
+    name,
+    size = 32,
+  } = defineProps<Props>()
+</script>
+
+<template>
+  <svg
+    class="icon__svg"
+    :height=size
+    :width=size
+    aria-hidden="true"
+    display="block"
+    fill="currentColor"
+    focusable="false"
+    preserveAspectRatio="xMidYMid meet"
+    viewBox="0 0 32 32"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <template      v-if="name === 'debug'"><DebugPath /></template>
+    <template v-else-if="name === 'keyboard'"><KeyboardPath /></template>
+    <template v-else-if="name === 'redo'"><RedoPath /></template>
+    <template v-else-if="name === 'share'"><SharePath /></template>
+    <template v-else-if="name === 'trash-can'"><TrashCanPath /></template>
+    <template v-else-if="name === 'undo'"><UndoPath /></template>
+  </svg>
+</template>
---
+++ src/helpers/type.ts
@@ -0,0 +1,11 @@
+type iconNameType =
+    'debug'
+  | 'keyboard'
+  | 'redo'
+  | 'share'
+  | 'trash-can'
+  | 'undo'
+
+export {
+  type iconNameType,
+}

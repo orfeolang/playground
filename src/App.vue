28f6ab662d481a9e522260d220a09b6ad3f739e0
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2025-06-07 01:02:39 -0400

    Separate out header

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,9 @@
 <script setup lang="ts">
-// import HelloWorld from './components/HelloWorld.vue'
-// import TheWelcome from './components/TheWelcome.vue'
+  import Header from './components/Header.vue'
 </script>
 
 <template>
-  <header>
-    <div>
-      <img src="/favicon.svg" alt="orfeo" height="30px" />
-      <button>compile</button>
-      <select name="compiler-version" id="compiler-version">
-        <option value="0.2.0">v0.2.0</option>
-        <!-- <option value="0.1.1">v0.1.1</option> -->
-      </select>
-      <select name="examples" id="examples">
-        <option value="example 1">example 1</option>
-        <option value="example 2">example 2</option>
-      </select>
-      <button>save/share</button>
-    </div>
-    <div>
-      <a href="https://orfeolang.com">Orfeo's website</a>
-    </div>
-  </header>
+  <Header/>
   <main>
     <div id="code-windows">
       <div id="editor" contenteditable="true">editor</div>
@@ -31,20 +13,6 @@
 </template>
 
 <style scoped>
-
-  header {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    height: 40px;
-    padding-inline: 5px;
-
-    div {
-      display: flex;
-      align-items: center;
-    }
-  }
-
   #code-windows {
     display: flex;
 
---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    <div>
+      <img src="/favicon.svg" alt="orfeo" height="30px" />
+      <button>compile</button>
+      <select name="compiler-version" id="compiler-version">
+        <option value="0.2.0">v0.2.0</option>
+        <!-- <option value="0.1.1">v0.1.1</option> -->
+      </select>
+      <select name="examples" id="examples">
+        <option value="example 1">example 1</option>
+        <option value="example 2">example 2</option>
+      </select>
+      <button>save/share</button>
+    </div>
+    <div>
+      <a href="https://orfeolang.com">Orfeo's website</a>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 40px;
+    padding-inline: 5px;
+
+    div {
+      display: flex;
+      align-items: center;
+    }
+  }
+</style>
